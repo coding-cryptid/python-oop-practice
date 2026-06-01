d33da272b46369ff31125fc88ba381488a6897f1
Gianna Cosenza <giacosey28@gmail.com>
2026-06-01 11:29:23 -0400

    Completed part 2: working with objects. Fixed methods to be under the student class

--- students.py
+++ students.py
@@ -4,24 +4,36 @@ class Student:
         self.name = name
         self.email = email
         self.grades = grades
+        
+    # Methods for the Student class
+    # Add a grade to the student's list of grades
+    def add_grade(self, grade):
+        self.grades.append(grade)
 
+    # Calculate the average grade for the student
+    def average_grade(self):
+        if len(self.grades) == 0:
+            return 0
+        return sum(self.grades) / len(self.grades)
 
-# Methods for the Student class
+    # Display information about the student
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Email: {self.email}")
+        print(f"Grades: {self.grades}")
+        print(f"Average Grade: {self.average_grade()}")
 
-# Add a grade to the student's list of grades
-def add_grade(self, grade):
-    self.grades.append(grade)
+# Created student objects with their name, email, and list of initial grades
+student1 = Student('John', 'jbschoolname@gmail.com', [85, 90, 78])
+student2 = Student('Jane', 'jaschoolname@gmail.com', [92, 88, 87])
+student3 = Student('Tim', 'tdscoolname@gmail.com', [90, 95, 100])
 
-# Calculate the average grade for the student
-def average_grade(self):
-    if len(self.grades) == 0:
-        return 0
-    return sum(self.grades) / len(self.grades)
+# Add a new grade for each student
+student1.add_grade(82)
+student2.add_grade(91)
+student3.add_grade(98)
 
-# Display information about the student
-def display_info(self):
-    print(f"Name: {self.name}")
-    print(f"Email: {self.email}")
-    print(f"Grades: {self.grades}")
-    print(f"Average Grade: {self.average_grade()}")
-
+# Display information about each student
+student1.display_info()
+student2.display_info()
+student3.display_info()
