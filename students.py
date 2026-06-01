c956195b561b0fcdba70e531991e59dd57ffb749
Gianna Cosenza <giacosey28@gmail.com>
2026-06-01 10:02:08 -0400

    Finished part one: class definition. defined add grade, average grade, and display info methods

--- students.py
+++ students.py
@@ -4,3 +4,16 @@ class Student:
         self.email = email
         self.grades = grades
 
+def add_grade(self, grade):
+    self.grades.append(grade)
+
+def average_grade(self):
+    if len(self.grades) == 0:
+        return 0
+    return sum(self.grades) / len(self.grades)
+
+def display_info(self):
+    print(f"Name: {self.name}")
+    print(f"Email: {self.email}")
+    print(f"Grades: {self.grades}")
+    print(f"Average Grade: {self.average_grade()}")
